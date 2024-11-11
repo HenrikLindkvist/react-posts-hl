8ccd99246becfa17c8fbcad11dac0b920eda1896
Henrik Lindkvist <henrik.lindkvist@gmail.com>
2024-11-11 15:07:05 +0100

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom"; // TODO Hash router
+// import { BrowserRouter as Router} from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom"; // For static page
 import { Provider } from "react-redux";
 import { store } from "./state/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </StrictMode>
 );
